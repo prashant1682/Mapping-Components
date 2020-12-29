da23eb0e712a075a5de3183108c153a30a7f2ff7
prashant1682 <prashant009@gmail.com>
2020-12-29 15:23:17 +0530

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,26 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+function createCard(contacts) {
+  // createCard fucntion is created and the information (objects) in the contacts.js is passed as custome properties.
+  return (
+    <Card
+      key={contacts.id}
+      id={contacts.id}
+      name={contacts.name}
+      img={contacts.imgURL}
+      tel={contacts.phone}
+      email={contacts.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {/* create a map of the contact.js and then pass it to a fucntion which will call the card componenet */}
+      {contacts.map(createCard)};
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,8 @@ function Card(props) {
   return (
     <div className="card">
       <div className="top">
+        {/* Cannot use the key property , so created a custom property of ID to tap in the value */}
+        <p>{props.id}</p>
         <h2 className="name">{props.name}</h2>
         <Avatar img={props.img} />
       </div>
